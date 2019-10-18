1e6f5b837bcfdd88df42ba5f8d3584606fc0047c
Tiger Wang <ztw17@ic.ac.uk>
2019-10-18 12:10:26 +0100

    Fix some issues in measuredistance, correct wrong assumptions about encoders

--- measuredistance.py
+++ measuredistance.py
@@ -4,29 +4,31 @@ import time
 import sys
 import brickpi3
 
+MM_DISTANCE = float(sys.argv[1])
+scaling_factor = 360/229
+scaled_distance = abs(MM_DISTANCE * scaling_factor)
+
 def get_motor_power(port):
     return BP.get_motor_status(port)[1]
 
 BP = brickpi3.BrickPi3()
-BP.set_motor_power(BP.PORT_B, 15)
-BP.set_motor_power(BP.PORT_C, 15)
 
-scaling_factor = 360/229
-MM_DISTANCE = int(sys.argv[1])
-scaled_distance = MM_DISTANCE * scaling_factor
+BP.set_motor_power(BP.PORT_B, 15 * (MM_DISTANCE / abs(MM_DISTANCE)))
+BP.set_motor_power(BP.PORT_C, 15 * (MM_DISTANCE / abs(MM_DISTANCE)))
 
-encoder_b_position = 0
-encoder_c_position = 0
+
+BP.reset_motor_encoder(BP.PORT_B)
+BP.reset_motor_encoder(BP.PORT_C)
+
 while True:
-    encoder_b_position += BP.get_motor_encoder(BP.PORT_B)
-    encoder_c_position += BP.get_motor_encoder(BP.PORT_C)
-    BP.reset_motor_encoder(BP.PORT_B)
-    BP.reset_motor_encoder(BP.PORT_C)
+    encoder_b_position = abs(BP.get_motor_encoder(BP.PORT_B))
+    encoder_c_position = abs(BP.get_motor_encoder(BP.PORT_C))
 
     print(encoder_b_position, encoder_c_position)
 
     if encoder_b_position > scaled_distance:
         BP.set_motor_power(BP.PORT_B, 0)
+    if encoder_c_position > scaled_distance:
         BP.set_motor_power(BP.PORT_C, 0)
     if (get_motor_power(BP.PORT_B) == 0 and get_motor_power(BP.PORT_C) == 0):
         break
