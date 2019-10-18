1a437c70281ab1cdfd1c6fc952d5d7a23be03590
Tiger Wang <ztw17@ic.ac.uk>
2019-10-18 12:58:31 +0100

    Try with checkpoints

--- measuredistance-abs.py
+++ measuredistance-abs.py
@@ -2,16 +2,10 @@
 
 import time
 import sys
-import brickpi3
+from common import *
 
 MM_DISTANCE = float(sys.argv[1])
-scaling_factor = 360/229
-scaled_distance = abs(MM_DISTANCE * scaling_factor)
-
-def get_motor_power(port):
-    return BP.get_motor_status(port)[1]
-
-BP = brickpi3.BrickPi3()
+scaled_distance = abs(MM_DISTANCE * ScalingFactors.movement)
 
 BP.reset_motor_encoder(BP.PORT_B)
 BP.reset_motor_encoder(BP.PORT_C)
@@ -19,7 +13,14 @@ BP.reset_motor_encoder(BP.PORT_C)
 BP.set_motor_limits(BP.PORT_B, 35)
 BP.set_motor_limits(BP.PORT_C, 35)
 
-BP.set_motor_position(BP.PORT_B, scaling_factor * MM_DISTANCE)
-BP.set_motor_position(BP.PORT_C, scaling_factor * MM_DISTANCE)
+final_encoder_pos = ScalingFactors.movement * MM_DISTANCE
+for pos in range(0, int(final_encoder_pos), 20):
+    BP.set_motor_position(BP.PORT_B, pos)
+    BP.set_motor_position(BP.PORT_C, pos)
+    while BP.get_motor_encoder(BP.PORT_B) != pos and BP.get_motor_encoder(BP.PORT_C) != pos:
+        time.sleep(0.02)
+
+BP.set_motor_position(BP.PORT_B, final_encoder_pos)
+BP.set_motor_position(BP.PORT_C, final_encoder_pos)
 
 
