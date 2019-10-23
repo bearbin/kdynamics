e6a17b2aca606e692909a6c8a9f1b6942fa39567
Tiger Wang <ztw17@ic.ac.uk>
2019-10-23 17:41:00 +0100

    Improve notes and changed comments

--- prac/LEGO-Motor_Position.py
+++ prac/LEGO-Motor_Position.py
@@ -23,24 +23,24 @@ BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be 
 
 try:
     try:
-        BP.offset_motor_encoder(BP.PORT_C, BP.get_motor_encoder(BP.PORT_C)) # reset encoder A
-        BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B)) # reset encoder D
+        BP.offset_motor_encoder(BP.PORT_C, BP.get_motor_encoder(BP.PORT_C)) # reset encoder C
+        BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B)) # reset encoder B
     except IOError as error:
         print(error)
     
-    BP.set_motor_power(BP.PORT_B, BP.MOTOR_FLOAT)    # float motor D
+    BP.set_motor_power(BP.PORT_B, BP.MOTOR_FLOAT)    # float motor B
     BP.set_motor_limits(BP.PORT_C, 50, 200)          # optionally set a power limit (in percent) and a speed limit (in Degrees Per Second)
     while True:
         # Each of the following BP.get_motor_encoder functions returns the encoder value.
         try:
-            target = BP.get_motor_encoder(BP.PORT_B) # read motor D's position
+            target = BP.get_motor_encoder(BP.PORT_B) # read motor B's position
         except IOError as error:
             print(error)
         
-        BP.set_motor_position(BP.PORT_C, target)    # set motor A's target position to the current position of motor D
+        BP.set_motor_position(BP.PORT_C, target)    # set motor C's target position to the current position of motor B
         
         try:
-            print("Motor C target: %6d  Motor C position: %6d" % (target, BP.get_motor_encoder(BP.PORT_C)))
+            print(("Motor C target: %6d  Motor C status: " % target), BP.get_motor_status(BP.PORT_C))
         except IOError as error:
             print(error)
         
