10a07bba5f59e8818161d953be1fc808e17b9cd6
Tiger Wang <ztw17@ic.ac.uk>
2019-11-07 10:22:58 +0000

    Calculate shortest m using functional programming

--- mcl.py
+++ mcl.py
@@ -3,36 +3,45 @@
 import sys
 from common import *
 from robomap import *
-from math import cos, sin
-
-MM_DISTANCE = float(sys.argv[1])
+from math import cos, sin, radians
 
 reset_encoders()
 
 set_limit_at(35)
 
 # gets distance to wall
-def calculate_m(float x, float y, (float ax, float ay, float bx, float by)):
-  return ((by - ay) * (ax - x) - (bx - ax) * (ay - y)) /
-         ((by - ay) * cos(theta) - (bx - ax) * sin(theta))
+def get_dist_to_wall(x, y, theta, wall):  
+  (ax, ay, bx, by) = wall
 
-def intersects_wall(float x, float y, (float ax, float ay, float bx, float by))
+  numerator = ((by - ay) * (ax - x) - (bx - ax) * (ay - y)) 
+  denominator = ((by - ay) * cos(theta) - (bx - ax) * sin(theta))
+  dist = numerator / denominator if not denominator == 0 else -1
+
+  print("The wall is ", wall, " with distance ", dist)
+  return dist
+
+def intersects_wall(x, y, wall): 
+  (ax, ay, bx, by) = wall
   left_x = min(ax, bx)
   right_x = max(ax, bx)
   top_y = max(ay, by)
   bottom_y = min(ay, by)
 
-  return left_x <= x and x <= right_x and
-         bottom_y <= y and y <= top_y
+  return left_x <= x and x <= right_x and bottom_y <= y and y <= top_y
 
-def find_closest_wall(float x, float y, float theta):
+def find_closest_wall(x, y, theta):
   mymap = Map.draw_robot_wars_map()
-  return min(filter(lambda: m -> m >= 0 and
-                    intersects_wall(x + m * cos(theta), y + m * sin(theta), wall),
-                    [calculate_m(x, y, wall) for wall in mymap.walls]))
+  
+  # Get list of (wall, distance) pairs
+  wms = [(wall, get_dist_to_wall(x, y, theta, wall)) for wall in mymap.walls]
 
+  # Filter out walls behind or to the side of us
+  filtered_wms = filter(lambda wm: wm[1] >= 0 and intersects_wall(x + wm[1] * cos(theta), y + wm[1] * sin(theta), wm[0]), wms) 
 
-def calculate_likelihood(float x, float y, float theta, float z):
-  m = find_closest_wall(x, y, theta)
-  print m
+  # Return wall with min distance m
+  return min(filtered_wms, key = lambda wm: wm[0])
+
+def calculate_likelihood(x, y, degrees, z):
+  m = find_closest_wall(x, y, radians(degrees))
+  print(m)
 
