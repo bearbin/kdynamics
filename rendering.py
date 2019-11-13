cefba7e91fd9c656f6251b938837d1bb0dcb06cc
Tiger Wang <ztw17@ic.ac.uk>
2019-11-13 12:29:50 +0000

    Draw map instead of grid for rendering

--- rendering.py
+++ rendering.py
@@ -1,29 +1,55 @@
-import time
+#import time
 import sys
 import brickpi3
 
 MAX_POINT_Y = 64 * 12
-COORDINATE_FRAME_CM = 210
-COORDINATE_FRAME_CM_X = 210
-COORDINATE_FRAME_CM_Y = 210
+COORDINATE_FRAME_CM = 230
+COORDINATE_FRAME_CM_X = 230
+COORDINATE_FRAME_CM_Y = 230
 COORDINATE_BIG_STEP = 50
 COORDINATE_SMALL_STEP = 10
 BIG_STEP_SIZE = 1
 SMALL_STEP_SIZE = 0.5
 SCALING_FACTOR_CM = MAX_POINT_Y / COORDINATE_FRAME_CM
 
-DRAW_OFFSET_X = 0
-DRAW_OFFSET_Y = 210
+DRAW_OFFSET_X = 10
+DRAW_OFFSET_Y = 220
 
 state = []
+
+def offsetCoordinatesTuple(coords):
+    return (coords[0] + DRAW_OFFSET_X, -coords[1] + DRAW_OFFSET_Y)
+
+def drawWalls():
+# Definitions of walls
+# a: O to A
+# b: A to B
+# c: C to D
+# d: D to E
+# e: E to F
+# f: F to G
+# g: G to H
+# h: H to O
+    drawLineTuple((0,0,0,168))        # a
+    drawLineTuple((0,168,84,168))     # b
+    drawLineTuple((84,126,84,210))    # c
+    drawLineTuple((84,210,168,210))   # d
+    drawLineTuple((168,210,168,84))   # e
+    drawLineTuple((168,84,210,84))    # f
+    drawLineTuple((210,84,210,0))     # g
+    drawLineTuple((210,0,0,0))        # h
 
 def drawLineTupleRaw(line):
     print("drawLine:" + str(line))
 
 def drawLineXYRaw(x0,y0,x1,y1):
-    drawLineTuple((x0,y0,x1,y1))
+    drawLineTuple((x0,y0,x1,y1), offset=False)
 
-def drawLineTuple(line):
+def drawLineTuple(line, offset=True):
+    if offset:
+        offsetStart = offsetCoordinatesTuple((line[0], line[1]))
+        offsetEnd = offsetCoordinatesTuple((line[2], line[3]))
+        line = (offsetStart[0], offsetStart[1], offsetEnd[0], offsetEnd[1])
     drawLineTupleRaw(tuple([x * SCALING_FACTOR_CM for x in line]))
 
 def drawLineXY(x0,y0,x1,y1):
@@ -55,6 +81,7 @@ def resetDrawingState():
     state = []
 
 def drawCoordinateFrame(withGrid):
+    return
     drawLineXY(0,0,0,COORDINATE_FRAME_CM_Y)
     drawLineXY(0,0,COORDINATE_FRAME_CM_X,0)
     for stepData in [[COORDINATE_BIG_STEP, BIG_STEP_SIZE], [COORDINATE_SMALL_STEP, SMALL_STEP_SIZE]]:
