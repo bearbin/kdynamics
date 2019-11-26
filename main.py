29a110fccdfa9847be80b86ec5ac37fcce1d083d
Tiger Wang <ztw17@ic.ac.uk>
2019-11-26 08:31:06 +0000

    Test Waypoints

--- main.py
+++ main.py
@@ -28,14 +28,14 @@ MAX_DESTROY_ADVANCE_CM = 100
 
 class Waypoints:
     class Path1:
-        SEEK: [[100, 100]]
-        RETALIATE: [[100, 100]]
+        SEEK: [[40, 60]]
+        RETALIATE: [INITIAL_POSITION]
     class Path2:
-        SEEK: [[100, 100]]
-        RETALIATE: [[100, 100]]
+        SEEK: [[100, 60]]
+        RETALIATE: [INITIAL_POSITION]
     class Path3:
-        SEEK: [[100, 100]]
-        RETALIATE: [[100, 100]]
+        SEEK: [[120, 40]]
+        RETALIATE: [INITIAL_POSITION]
 
 ########################## FUNCTIONS ############################
 
@@ -56,28 +56,40 @@ def ROBOT_do_wall_folowing_approach():
 
 ##################################################################
 
+def SONAR_locate_target():
+    return 0
+
+
 def ROBOT_seek_and_destroy():
     left_rotation_angle_radians = SONAR_locate_target()
+
     ROBOT_rotate_left_radians(left_rotation_angle_radians)
     ROBOT_move_forward_with_bump_check(MAX_DESTROY_ADVANCE_CM)
 
 
 def ROBOT_destroy(waypoints_to_seek, waypoints_to_retaliate):
+    waypoints_to_seek = path_info.SEEK
+    waypoints_to_retaliate = path_info.RETALIATE
+
     ROBOT_do_waypoints(waipoints_to_seek)
     ROBOT_seek_and_destroy()
     ROBOT_do_waypoints(waypoints_to_retaliate)
 
 
 def ROBOT_destroy_first():
-    ROBOT_destroy(Waypoints.Path1.SEEK, Waypoints.Path1.RETALIATE)
+    ROBOT_destroy(Waypoints.Path1)
 
 
 def ROBOT_destroy_second():
-    ROBOT_destroy(Waypoints.Path2.SEEK, Waypoints.Path2.RETALIATE)
+    ROBOT_destroy(Waypoints.Path2)
 
 
 def ROBOT_destroy_third():
-    ROBOT_destroy(Waypoints.Path3.SEEK, Waypoints.Path3.RETALIATE)
+    ROBOT_destroy(Waypoints.Path3)
+
+
+def ROBOT_return_to_origin():
+    ROBOT_go_to_waypoint(INITIAL_POSITION)
 
 
 def ROBOT_do_seek_and_destroy_approach():
@@ -91,4 +103,4 @@ def ROBOT_do_seek_and_destroy_approach():
 drawWorld()
 ROBOT_initialize_waypoint_state(INITIAL_X, INITIAL_Y, INITIAL_ANGLE)
 
-
+ROBOT_do_seek_and_destoy_approach()
